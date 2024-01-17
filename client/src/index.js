7e25cdb7243fb132bc9968341a10da3a0e92a0dc
Thomas DA ROCHA <thomas.darocha@lenra.io>
2024-01-17 14:39:17 +0100

    fix: Style

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,10 @@
 import { LenraApp } from '@lenra/client';
 
+const subsGoal = 15;
+
+const subsProgress = document.getElementById("subsProgress");
+subsProgress.querySelector("strong").innerHTML = subsGoal;
+
 const app = new LenraApp({
     clientId: "XXX-XXX-XXX",
 });
@@ -12,5 +17,6 @@ app.connect().then(() => {
         outputs[1].innerHTML = data.lastFollower;
         outputs[2].innerHTML = data.donation;
         outputs[3].innerHTML = data.subscriptionCount;
+        subsProgress.style.setProperty('--progress', `${data.subscriptionCount / subsGoal}`);
     });
 });
