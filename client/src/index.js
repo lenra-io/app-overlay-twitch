6853f567cb13a6d5e3e5c21a16cb0de673a64ddf
Thomas DA ROCHA <thomas.darocha@lenra.io>
2024-04-18 15:53:53 +0200

    feat: Public access

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,13 @@ subsProgress.querySelector("strong").innerHTML = subsGoal;
 
 const app = new LenraApp({
     clientId: "9f3fbb6c-c865-4d98-b815-4affb3c24bee",
+    appName: "d73f924c-9358-4c59-ab56-6a74ec13d4c1",
     redirectUri: window.location.href.replace(/\/(index\.html(\?.*))?$/, "/redirect.html"),
     isProd: true,
 });
 
-app.connect().then(() => {
-    const route = app.route(`/data`, (data) => {
+app.openSocket().then(() => {
+    app.route(`/data`, (data) => {
         let outputs = document.getElementsByTagName("output");
         console.log("Received data", data);
         outputs[0].innerHTML = data.lastSubscriber;
